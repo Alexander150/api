74d70f834bffa76500543ae6ec1c2a2097a978cc
zarnik119 <zarnik119@gmail.com>
2019-03-23 17:51:30 +0200

    sdfghjk

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,10 +1,13 @@
+require 'net/http'
+require 'json'
+require 'uri'
 class PostController < ApplicationController
   def index
   end
 
   def new
   	app_id = '6907700'
-  	
+
   	auth = URI('https://oauth.vk.com/authorize?v=5.92&client_id=' + app_id + '&redirect_uri=https://intense-stream-65954.herokuapp.com')
 	ans = JSON.parse(Net::HTTP.get(auth))
 	print(auth)
