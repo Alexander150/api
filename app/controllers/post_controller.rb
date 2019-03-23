864c9fc7fbfd2d6668591836dea417784a173bf4
zarnik119 <zarnik119@gmail.com>
2019-03-23 18:03:06 +0200

    asd

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -10,6 +10,6 @@ class PostController < ApplicationController
 
   	auth = URI('https://oauth.vk.com/authorize?v=5.92&client_id=' + app_id + '&redirect_uri=https://intense-stream-65954.herokuapp.com')
 	ans = JSON.parse(Net::HTTP.get(auth))
-	print(auth)
+	print(ans)
   end
 end
